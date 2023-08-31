a60902d9ca3203fafa5686d0d2d45d3e82a1f80a
William M. Tsikata <tsikatawill@gmail.com>
2023-08-31 18:08:46 +0000

    populate homescreen
    - add header
    - add brandlogos section
    - new arrivals section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import CustomHeading from "@/components/CustomHeading";
-import CustomText from "@/components/CustomText";
+import BrandLogos from "@/components/BrandLogos";
+import Header from "@/components/Header";
+import NewArrivalsSection from "@/components/NewArrivalsSection";
 
 export default async function Home() {
   return (
-    <main className="min-h-screen justify-between p-24 space-y-2">
-      <CustomHeading variant="h2" className="underline">
-        Acheter
-      </CustomHeading>
-      <CustomText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
-        perferendis. Assumenda sequi facere repellendus repellat culpa ducimus!
-        Nostrum consectetur, veniam sequi corporis odit tenetur suscipit,
-        voluptates maxime laboriosam, ullam fugit!
-      </CustomText>
-    </main>
+    <>
+      <Header />
+      <BrandLogos />
+      <NewArrivalsSection />
+    </>
   );
 }
