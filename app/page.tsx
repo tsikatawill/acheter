2de4aff3da95a29cd19462e5606865ca97e0a3a2
William M. Tsikata <tsikatawill@gmail.com>
2023-08-31 12:09:22 +0000

    add custom text & heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
+import CustomHeading from "@/components/CustomHeading";
+import CustomText from "@/components/CustomText";
+
 export default async function Home() {
   return (
-    <main className="min-h-screen justify-between p-24 space-y-5">
-      <h2 className="font-bold text-3xl underline underline-offset-8">
+    <main className="min-h-screen justify-between p-24 space-y-2">
+      <CustomHeading variant="h2" className="underline">
         Acheter
-      </h2>
-      <p>
+      </CustomHeading>
+      <CustomText>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
         perferendis. Assumenda sequi facere repellendus repellat culpa ducimus!
         Nostrum consectetur, veniam sequi corporis odit tenetur suscipit,
         voluptates maxime laboriosam, ullam fugit!
-      </p>
+      </CustomText>
     </main>
   );
 }
